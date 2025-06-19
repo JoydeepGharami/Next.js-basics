409d40ef9ec198814637543208174b9d34a38563
Joydeep_@-2003 <157727146+Joydeep663-2003@users.noreply.github.com>
2025-06-19 12:23:16 +0530

    04

---
+++ db.js
@@ -0,0 +1,18 @@
+export const users = [{
+    "id":1,
+    "username":"joydeep"
+},
+{
+    "id":2,
+    "username":"joy"
+},
+{
+
+    "id":3,
+    "username":"john"
+},
+{
+    "id":4,
+    "username":"roman"
+}
+]
